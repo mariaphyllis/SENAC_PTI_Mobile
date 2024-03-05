2c091e1a1a3d881d520a3f4fe654b9327069e74c
Maria Phyllis <75624308+mariaphyllis@users.noreply.github.com>
2024-03-04 21:49:57 -0300

    Create PTI.jsx

---
+++ PTI.js
@@ -0,0 +1,34 @@
+const menu = [
+    { nome: "Beef burger", calorias: 70, preco: 12.0,
+    logoUri:"https://beefburger.png"},
+    { nome: "Pancake", calorias: 60, preco: 11.0,
+    logoUri:"https://pancake.png"}];
+    
+    function Card({menu}) {
+    return (
+    <View style={styles.cardContainer}>
+    <View>
+    <Text>{menu.nome}</Text>
+    <Text>{menu.calorias} calorias</Text>
+    <Text>$ {menu.preco}</Text>
+    </View>
+    <Image
+    accessibilityLabel={'imagem de '+menu.nome}
+    source={{ uri: menu.logoUri }}
+    style={styles.logo}
+    />
+    </View>
+    );
+    }
+    
+    function App() {
+    return (
+    
+    <View>
+    <Card menu={menu[0]}></Card>
+    <Card menu={menu[1]}></Card>
+    </View>
+    );
+    }
+    export default App;
+    */
